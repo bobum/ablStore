670caa72acff67e6f95d28c21edf7d70290c97ee
scott davis <scott.davis@semaphoredoes.com>
2016-10-01 21:16:07 -0400

    modifying port for heroku test

--- passportServer.js
+++ passportServer.js
@@ -126,4 +126,7 @@ app.get('/newUser', function(req, res) {
 });
 
 app.use(express.static('public'));
-app..listen(process.env.PORT || 3000);
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
