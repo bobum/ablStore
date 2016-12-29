828f4a610e9ad9054cf1d466456a3f0ef8a41a38
bobum <scott@davisplanet.com>
2016-12-28 22:43:18 -0500

    trying redshift

--- passportServer.js
+++ passportServer.js
@@ -127,6 +127,11 @@ app.get('/newUser', function(req, res) {
 
 app.use(express.static('public'));
 app.set('port', (process.env.PORT || 3000));
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+//app.listen(app.get('port'), function() {
+//  console.log('Node app is running on port', app.get('port'));
+//});
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+module.exports = app ;
